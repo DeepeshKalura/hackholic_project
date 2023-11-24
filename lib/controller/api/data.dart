349dc1777291c86147050d23a0604b866ded7758
DeepeshKalura <deepeshkalurs@gmail.com>
2023-11-24 13:29:52 +0530

    dummy_data

---
+++ lib/controller/api/data.dart
@@ -0,0 +1,136 @@
+import '../../model/models.dart';
+
+final UserModel currentUser = UserModel(
+  uid: '1',
+  email: 'marcusng@email.com',
+  name: 'Marcus Ng',
+  imageUrl:
+      'https://images.unsplash.com/photo-1578133671540-edad0b3d689e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80',
+);
+
+final List<UserModel> onlineUsers = [
+  UserModel(
+      uid: '2',
+      name: 'David Brooks',
+      imageUrl:
+          'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+      email: "davidbooks@email.com"),
+  UserModel(
+      uid: '3',
+      name: 'Jane Doe',
+      imageUrl:
+          'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+      email: "janedoe@email.com"),
+  UserModel(
+      uid: '4',
+      name: 'Matthew Hinkle',
+      imageUrl:
+          'https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1331&q=80',
+      email: "mattewHinkle@gmail.com"),
+];
+
+final List<StoryModel> stories = [
+  StoryModel(
+      user: onlineUsers[2],
+      isViewed: false,
+      imageUrl:
+          'https://images.unsplash.com/photo-1498307833015-e7b400441eb8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80',
+      storyId: '1',
+      storyTitle: "My Story"),
+  StoryModel(
+      user: onlineUsers[2],
+      imageUrl:
+          'https://images.unsplash.com/photo-1499363536502-87642509e31b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+      isViewed: true,
+      storyId: '2',
+      storyTitle: "My Story two"),
+  StoryModel(
+      user: onlineUsers[0],
+      imageUrl:
+          'https://images.unsplash.com/photo-1497262693247-aa258f96c4f5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=624&q=80',
+      isViewed: false,
+      storyId: '3',
+      storyTitle: "My Story three"),
+  StoryModel(
+      user: onlineUsers[1],
+      imageUrl:
+          'https://images.unsplash.com/photo-1496950866446-3253e1470e8e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+      isViewed: true,
+      storyId: '4',
+      storyTitle: "My Story four")
+];
+
+final List<PostModel> posts = [
+  PostModel(
+    user: currentUser,
+    caption: 'Check out these cool puppers',
+    timeAgo: '58m',
+    imageUrl: 'https://images.unsplash.com/photo-1525253086316-d0c936c814f8',
+    likes: 1202,
+    comments: 184,
+    shares: 96,
+    description: "Blah! Blah! Blah!",
+    postId: "1",
+  ),
+  PostModel(
+    user: onlineUsers[2],
+    caption:
+        'Please enjoy this placeholder text: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    timeAgo: '3hr',
+    imageUrl: 'https://images.unsplash.com/photo-1525253086316-d0c936c814f8',
+    likes: 683,
+    comments: 79,
+    shares: 18,
+    description: "Blah! Blah! Blah!",
+    postId: "2",
+  ),
+  PostModel(
+    user: onlineUsers[1],
+    caption: 'This is a very good boi.',
+    timeAgo: '8hr',
+    imageUrl:
+        'https://images.unsplash.com/photo-1575535468632-345892291673?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+    likes: 894,
+    comments: 201,
+    shares: 27,
+    description: "Blah! Blah! Blah!",
+    postId: "3",
+  ),
+  PostModel(
+    user: onlineUsers[0],
+    caption: 'Adventure 🏔',
+    timeAgo: '15hr',
+    imageUrl:
+        'https://images.unsplash.com/photo-1573331519317-30b24326bb9a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+    likes: 722,
+    comments: 183,
+    shares: 42,
+    description: "Blah! Blah! Blah!",
+    postId: "4",
+  ),
+  PostModel(
+    user: onlineUsers[1],
+    caption:
+        'More placeholder text for the soul: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    timeAgo: '1d',
+    imageUrl:
+        "https://images.unsplash.com/photo-1573331519317-30b24326bb9a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    likes: 482,
+    comments: 37,
+    shares: 9,
+    description: "Blah! Blah! Blah!",
+    postId: "5",
+  ),
+  PostModel(
+    user: onlineUsers[2],
+    caption: 'A classic.',
+    timeAgo: '1d',
+    imageUrl:
+        'https://images.unsplash.com/reserve/OlxPGKgRUaX0E1hg3b3X_Dumbo.JPG?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+    likes: 1523,
+    shares: 129,
+    comments: 301,
+    description: "Blah! Blah! Blah!",
+    postId: "6",
+  )
+];
