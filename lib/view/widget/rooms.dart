6f70cc1eaa0f9e41b06bee7482300dd8ec15351b
DeepeshKalura <deepeshkalurs@gmail.com>
2023-11-24 13:30:31 +0530

    Add Rooms widget for displaying online users and create room button

---
+++ lib/view/widget/rooms.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+import '../../model/user_model.dart';
+import '../theme/app_theme.dart';
+import 'profile_avatar.dart';
+import 'responsive.dart';
+
+class Rooms extends StatelessWidget {
+  final List<UserModel> onlineUsers;
+
+  const Rooms({
+    Key? key,
+    required this.onlineUsers,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isDesktop = Responsive.isDesktop(context);
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: isDesktop ? 5.0 : 0.0),
+      elevation: isDesktop ? 1.0 : 0.0,
+      shape: isDesktop
+          ? RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0))
+          : null,
+      child: Container(
+        height: 60.0,
+        color: Colors.white,
+        child: ListView.builder(
+          padding: const EdgeInsets.symmetric(
+            vertical: 10.0,
+            horizontal: 4.0,
+          ),
+          scrollDirection: Axis.horizontal,
+          itemCount: 1 + onlineUsers.length,
+          itemBuilder: (BuildContext context, int index) {
+            if (index == 0) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: _CreateRoomButton(),
+              );
+            }
+            final UserModel user = onlineUsers[index - 1];
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: ProfileAvatar(
+                imageUrl: user.imageUrl,
+                isActive: true,
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class _CreateRoomButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton.icon(
+        icon: const Icon(
+          Icons.video_call,
+          size: 35.0,
+          color: Colors.purple,
+        ),
+        label: const Text('Create\nRoom',
+            style: TextStyle(color: Palette.facebookBlue)),
+        onPressed: () => print('Create Room'),
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all(Colors.white),
+          shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+          ),
+        ));
+  }
+}
