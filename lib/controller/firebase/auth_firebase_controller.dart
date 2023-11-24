ad1c63c9642cd6496406e595d3fc27131985c842
DeepeshKalura <deepeshkalurs@gmail.com>
2023-11-24 16:47:07 +0530

    Add authentication functionality to Firebase controller

--- lib/controller/firebase/auth_firebase_controller.dart
+++ lib/controller/firebase/auth_firebase_controller.dart
@@ -1,1 +1,32 @@
+import 'package:firebase_auth/firebase_auth.dart';
 
+class AuthFirebaseController {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  Future<String> signUp(String email, String password) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return "Success";
+    } on FirebaseAuthException catch (e) {
+      return e.message!;
+    }
+  }
+
+  Future<String> signIn(String email, String password) async {
+    try {
+      await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return "Success";
+    } on FirebaseAuthException catch (e) {
+      return e.message!;
+    }
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+}
