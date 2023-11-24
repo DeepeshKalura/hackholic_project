86243b59e1897f3896bd16a84460f9248a399357
DeepeshKalura <deepeshkalurs@gmail.com>
2023-11-24 16:46:30 +0530

    Add RandomUrlImageController class for generating random profile image URLs

---
+++ lib/controller/api/random_url_image_controller.dart
@@ -0,0 +1,15 @@
+import 'dart:math' as math;
+
+class RandomUrlImageController {
+  List<String> profileUrl = [
+    "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/1542085/pexels-photo-1542085.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/3763188/pexels-photo-3763188.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  ];
+  String random() {
+    var rng = math.Random();
+    return profileUrl[rng.nextInt(profileUrl.length)];
+  }
+}
