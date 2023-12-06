fb9c01abad42601818763635954d21d7c35ffca3
DeepeshKalura <deepeshkalurs@gmail.com>
2023-12-06 15:59:08 +0530

    Add logging for FirebaseAuthExceptions

--- lib/controller/firebase/auth/email_auth_firebase_controller.dart
+++ lib/controller/firebase/auth/email_auth_firebase_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:developer' as developer;
+
 import 'package:firebase_auth/firebase_auth.dart';
 
 class EmailAuthFirebaseController {
@@ -5,35 +7,51 @@ class EmailAuthFirebaseController {
 
   EmailAuthFirebaseController({required this.auth});
 
+  Stream<User?> get user => auth.authStateChanges();
+
   Future<String> signUp(
     String email,
     String password,
   ) async {
     try {
-      var result = await auth.createUserWithEmailAndPassword(
-        email: email,
-        password: password,
+      await auth.createUserWithEmailAndPassword(
+        email: email.trim(),
+        password: password.trim(),
       );
 
-      return result.user!.uid;
+      return "Success";
     } on FirebaseAuthException catch (e) {
-      return e.message!;
+      developer.log(e.message ?? "FirebaseAuthException has the issue");
+      return "FirebaseAuthException has the issue";
+    } catch (e) {
+      developer.log(e.toString());
+      return "Catch has the issue";
     }
   }
 
   Future<String> signIn(String email, String password) async {
     try {
       await auth.signInWithEmailAndPassword(
-        email: email,
-        password: password,
+        email: email.trim(),
+        password: password.trim(),
       );
       return "Success";
     } on FirebaseAuthException catch (e) {
-      return e.message!;
+      developer.log(e.message ?? "FirebaseAuthException has the issue");
+      return "FirebaseAuthException has the issue";
+    } catch (e) {
+      developer.log(e.toString());
+      return "Catch has the issue";
     }
   }
 
   Future<void> signOut() async {
-    await auth.signOut();
+    try {
+      await auth.signOut();
+    } on FirebaseAuthException catch (e) {
+      developer.log(e.message ?? "FirebaseAuthException has the issue");
+    } catch (e) {
+      developer.log(e.toString());
+    }
   }
 }
